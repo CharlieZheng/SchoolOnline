94a81d7369db89cd33ff252fb8f8875fbb32544c
东莞市浙金信息科技有限公司Ubuntu <740483802@qq.com>
2018-06-01 10:13:24 +0800

    Add forms.py

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,18 +1,26 @@
 '''Views for Login'''
 
+# users/views.py
+
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
-# Create your views here.
 from django.shortcuts import render
+from django.views.generic.base import View
 
+from .forms import LoginForm
 from .models import UserProfile
-
-
+'''
 # 邮箱和用户名都可以登录
 # 基础ModelBackend类，因为它有authenticate方法
+'''
+
+
 class CustomBackend(ModelBackend):
+    '''todo'''
+
     def authenticate(self, request, username=None, password=None, **kwargs):
+        '''todo'''
         try:
             # 不希望用户存在两个，get只能有一个。两个是get失败的一种原因 Q为使用并集查询
             user = UserProfile.objects.get(
@@ -22,25 +30,36 @@ class CustomBackend(ModelBackend):
             # UserProfile继承的AbstractUser中有def check_password(self, raw_password):
             if user.check_password(password):
                 return user
-        except Exception as e:
+        except Exception:
             return None
 
 
-def user_login(request):
-    '''Login view'''
-    if request.method == 'POST':
-        # 获取用户提交的用户名和密码
-        user_name = request.POST.get('username', None)
-        pass_word = request.POST.get('password', None)
-        # 成功返回user对象,失败None
-        user = authenticate(username=user_name, password=pass_word)
-        # 如果不是null说明验证成功
-        if user is not None:
-            # 登录
-            login(request, user)
-            return render(request, 'index.html')
-        else:
-            return render(request, 'login.html', {'msg': '用户名或密码错误'})
+class LoginView(View):
+    '''todo'''
 
-    elif request.method == 'GET':
+    def get(self, request):
+        '''todo'''
         return render(request, 'login.html')
+
+    def post(self, request):
+        '''todo'''
+        # 实例化
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            # 获取用户提交的用户名和密码
+            user_name = request.POST.get('username', None)
+            pass_word = request.POST.get('password', None)
+            # 成功返回user对象,失败None
+            user = authenticate(username=user_name, password=pass_word)
+            # 如果不是null说明验证成功
+            if user is not None:
+                # 登录
+                login(request, user)
+                return render(request, 'index.html')
+            # 只有当用户名或密码不存在时，才返回错误信息到前端
+            else:
+                return render(request, 'login.html', {'msg': '用户名或密码错误', 'login_form': login_form})
+
+        # form.is_valid（）已经判断不合法了，所以这里不需要再返回错误信息到前端了
+        else:
+            return render(request, 'login.html', {'login_form': login_form})
