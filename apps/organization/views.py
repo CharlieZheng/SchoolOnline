0125927384af9a6d11e0f48b995b9a4ba4a99fbc
东莞市浙金信息科技有限公司Ubuntu <740483802@qq.com>
2018-06-02 10:23:23 +0800

    Add "category" parameter for org-list.html

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,3 +1,4 @@
+'''Router for org-list.html'''
 from django.shortcuts import render
 from django.views.generic.base import View
 from pure_pagination import PageNotAnInteger, Paginator
@@ -11,12 +12,22 @@ class OrgView(View):
     '''课程机构'''
 
     def get(self, request):
+        '''Render'''
         # 所有课程机构
         all_orgs = CourseOrg.objects.all()
+        # 类别筛选
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
         # 有多少家机构
         org_nums = all_orgs.count()
         # 所有城市
         all_citys = CityDict.objects.all()
+
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
         # 对课程机构进行分页
         # 尝试获取前台get请求传递过来的page参数
         # 如果是不合法的配置参数默认返回第一页
@@ -25,11 +36,13 @@ class OrgView(View):
         except PageNotAnInteger:
             page = 1
         # 这里指从allorg中取五个出来，每页显示5个
-        p = Paginator(all_orgs, 5, request=request)
-        orgs = p.page(page)
+        fewfew = Paginator(all_orgs, 2, request=request)
+        orgs = fewfew.page(page)
 
         return render(request, "org-list.html", {
             "all_orgs": orgs,
             "all_citys": all_citys,
+            "category": category,
             "org_nums": org_nums,
+            'city_id': city_id,
         })
